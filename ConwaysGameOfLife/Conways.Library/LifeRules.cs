4852783691a374d21910d3a2bfa93e07ca99c74a
AlexKimmig <alexander.kimmig@siemens.com>
2018-01-16 16:05:49 +0100

    Implemented Game Rules

--- ConwaysGameOfLife/Conways.Library/LifeRules.cs
+++ ConwaysGameOfLife/Conways.Library/LifeRules.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Conways.Library
 {
+    public enum CellState
+    {
+        Alive,
+        Dead,
+    }
     public class LifeRules
     {
+        public static CellState GetNewState(CellState currentState, int liveNeighbors)
+        {
+            if (currentState == CellState.Alive && liveNeighbors < 2) return CellState.Dead;
+            if (currentState == CellState.Alive && liveNeighbors > 3) return CellState.Dead;
+            if (currentState == CellState.Dead && liveNeighbors == 3) return CellState.Alive;
+            return currentState;
+        }
     }
 }
